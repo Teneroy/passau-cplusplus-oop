a01e6d3b8552f1b35a25ffe09e1e3d8e0d206208
Teneroy <alexchernishof@gmail.com>
2021-10-24 19:53:00 +0200

    Added first assignment answers

---
+++ cmake-build-debug/CMakeFiles/passau_cplusplus_oop.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/passau_cplusplus_oop.dir/main.cpp.o"
+  "passau_cplusplus_oop"
+  "passau_cplusplus_oop.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/passau_cplusplus_oop.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    /*int a,b;
+    cin >> a >> b;
+    if(a == b) {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }*/
+
+//    auto a = 0;
+//    cin >> a;
+
+    int a = 12;
+    int &b = a;
+
+    return 0;
+}
