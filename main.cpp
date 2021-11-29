f2b23b19d8f6dfe6dbdc16902b4ee959b86e3982
Teneroy <alexchernishof@gmail.com>
2021-11-29 18:48:40 +0100

    Added mult parsing

--- main.cpp
+++ main.cpp
@@ -4,7 +4,6 @@
 
 #include <iostream>
 #include "./question1/expr.h"
-
 using namespace std;
 
 //void rotate(int &a, int &b, int &c) {
@@ -22,13 +21,26 @@ int main() {
     Data four(4);
     Nil nil;
 
-    BlankNode blankNode5(four, nil);
-    BlankNode blankNode4(three, blankNode5);
-    BlankNode blankNode3(plus, blankNode4);
-    BlankNode blankNode2(blankNode3, nil);
-    BlankNode blankNode1(two, blankNode2);
-    BlankNode blankNode(mult, blankNode1);
-    cout << blankNode.str() << endl;
+//    BlankNode blankNode5(four, nil);
+//    BlankNode blankNode4(three, blankNode5);
+//    BlankNode blankNode3(plus, blankNode4);
+//    BlankNode blankNode2(blankNode3, nil);
+//    BlankNode blankNode1(two, blankNode2);
+//    BlankNode blankNode(mult, blankNode1);
+//    cout << blankNode.str() << endl;
+
+
+//    char string[80] = "5*2-3*2";
+//    // Make expression
+//    auto * eptr = new Express(string);
+//    // Parse it
+//    eptr->parse();
+//    // Solve the expression
+//    cout << eptr->solve() << endl;
+
+    cout << BuildAST::build("23*400")->str() << endl;
+    //BuildAST::build("23*(300+4)+5*(2+(32*3))");
+    //BuildAST::build("3*2+4*2+3");
 
     return 0;
 }
--- question1/expr.h
+++ question1/expr.h
@@ -5,6 +5,9 @@
 #ifndef PASSAU_CPLUSPLUS_OOP_EXPR_H
 #define PASSAU_CPLUSPLUS_OOP_EXPR_H
 #include <string>
+#include <iostream>
+
+using namespace std;
 
 class ASTNode {
 public:
@@ -14,18 +17,21 @@ public:
 
 class BlankNode : public ASTNode {
 public:
-    BlankNode(ASTNode &nodeLeft, ASTNode &nodeRight) : left{nodeLeft}, right{nodeRight} {};
+    BlankNode(ASTNode *nodeLeft, ASTNode *nodeRight){
+        left = nodeLeft;
+        right = nodeRight;
+    };
     std::string str() override {
         std::string result;
-        result.append(left.str());
-        result.append(right.str());
+        result.append(left -> str());
+        result.append(right -> str());
         return result;
     }
     int evaluate() override { return 0; }
 
 protected:
-    ASTNode &left;
-    ASTNode &right;
+    ASTNode *left;
+    ASTNode *right;
 };
 
 class Operator : public ASTNode {
@@ -69,6 +75,85 @@ public:
     int evaluate() override {
         return 0.0;
     };
+};
+
+class BuildAST {
+public:
+    static ASTNode * build(std::string expr) {
+//        cout << expr << endl;
+//        cout << expr.find('*') << endl;
+//        cout << expr.find('+') << endl;
+//        cout << expr.find('(') << endl;
+//        cout << expr.find(')') << endl;
+//
+//        int brPos = expr.find('(');
+//
+//
+//        int start_exp = 0;
+//        int stop_exp = 0;
+//        int i = 0;
+//        ASTNode * rootNode = nullptr;
+//        ASTNode * leftExpression = nullptr;
+//        ASTNode * rightExpression = nullptr;
+//        ASTNode * curNode = nullptr;
+//        string number;
+//        for (char cur : expr) {
+//            switch (cur) {
+//                case '*':
+//                    cout << "number: " << number << endl;
+//                    number = "";
+//                    if(leftExpression == nullptr) {
+//                        leftExpression = new Data(stoi(number));
+//                    }
+//                    Operator * mult = new Operator('*');
+//                    rootNode = new BlankNode(mult, new BlankNode(leftExpression, build()));
+//                    break;
+//                case '+':
+//                    cout << "number: " << number << endl;
+//                    number = "";
+//                    break;
+//            }
+//
+//            if (cur >= '0' && cur <= '9') {
+//                number.append(1, cur);
+//            }
+//
+//
+//            i++;
+//        }
+
+        return evalGenerate(expr);
+    }
+
+private:
+    static ASTNode * evalGenerate(string expr) {
+        ASTNode * rootNode = nullptr;
+        string number;
+        for (int i = 0; i < expr.length(); i++) {
+            if(expr.at(i) == '*') {
+                ASTNode * mult = new Operator('*');
+                ASTNode * data = new Data(stoi(number));
+                ASTNode * rightNode = new BlankNode(data, evalGenerate(expr.substr(i + 1)));
+                rootNode = new BlankNode(mult, rightNode);
+                number = "";
+                i = expr.length();
+                continue;
+            }
+
+            if(expr.at(i) >= '0' && expr.at(i) <= '9') {
+                number.append(1, expr.at(i));
+            }
+        }
+        if(rootNode != nullptr) {
+            return rootNode;
+        }
+
+        if(number.length() == 0) {
+            return new Nil;
+        }
+
+        return new BlankNode(new Data(stoi(number)), new Nil);
+    }
 };
 
 
