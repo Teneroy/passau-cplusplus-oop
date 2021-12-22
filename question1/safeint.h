c3a75a30adc7a5e26734820867831e4c3e8f4a01
Teneroy <alexchernishof@gmail.com>
2021-12-23 00:07:16 +0100

    Added first task

---
+++ question1/safeint.h
@@ -0,0 +1,28 @@
+//
+// Created by studone on 12/21/21.
+//
+
+#ifndef PASSAU_CPLUSPLUS_OOP_SAFEINT_H
+#define PASSAU_CPLUSPLUS_OOP_SAFEINT_H
+
+
+#include <stdexcept>
+#include <limits>
+
+class SafeInt {
+private:
+    int value;
+public:
+    SafeInt(int value) : value(value) {};
+    SafeInt(const SafeInt& obj);
+    friend std::ostream& operator << (std::ostream& os, const SafeInt& val);
+    friend SafeInt operator + (const SafeInt& val1, const SafeInt& val2);
+    friend SafeInt operator - (const SafeInt& val1, const SafeInt& val2);
+    friend SafeInt operator * (const SafeInt& val1, const SafeInt& val2);
+    SafeInt& operator++();
+    int operator()() const;
+    // Define the functions here
+};
+
+
+#endif //PASSAU_CPLUSPLUS_OOP_SAFEINT_H
