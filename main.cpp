a1bba29ac116d7d71cc1f47213a53bd59deb5c15
Teneroy <alexchernishof@gmail.com>
2021-12-28 23:07:36 +0100

    Implemented task 3

--- main.cpp
+++ main.cpp
@@ -4,8 +4,41 @@
 
 #include <iostream>
 #include <vector>
+#include "./question2/queue.h"
 using namespace std;
 
 int main() {
+    queue::Queue<int, 2> q;
+    for (int i = 0; i < 3; i++)
+    {
+        try
+        {
+            q.enqueue(67+i);
+        }
+        catch (queue::QOverflow &e)
+        {
+            std::cout << "Queue overflow" << std::endl;
+        }
+    }
+    for (int i = 0; i < 2; i++)
+    {
+        try
+        {
+            std::cout << q.dequeue() << std::endl;
+        }
+        catch (queue::QUnderflow &e)
+        {
+            std::cout << "Queue is empty" << std::endl;
+        }
+    }
+    std::cout << q << std::endl;
+    std::cout << q.size() << std::endl;
+
+    q.enqueue(67);
+    q.enqueue(68);
+
+    // call the function here to cast the queue
+    cout << q.cast<float>().dequeue() / 2 << endl;
+    cout << q.dequeue() / 2 << endl;
     return 0;
 }
